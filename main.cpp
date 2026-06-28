0c26b6ca37ce0f4ed56e54b3589207d52429e623
smokingLasagna <furanetsgeorgiy@gmail.com>
2026-06-29 01:55:38 +0300

    Second commit, added test.py

--- main.cpp
+++ main.cpp
@@ -1,8 +1,15 @@
 #include <iostream>
 using namespace std;
 
+int add(int a, int b)
+{
+	// Function added in second V
+	return a + b;
+}
+
 int main()
 {
 	cout << "This is first version of main.cpp\n";
+	cout << add(2, 3) << "\n";
 	return 0;
 }
